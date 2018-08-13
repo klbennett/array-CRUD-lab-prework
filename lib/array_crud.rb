3780b770cca0e3fea90cc94983488a67c70be966
Kay B <kaylouisebennett@gmail.com>
2018-08-13 13:29:31 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,11 +12,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array.unshift "Sandra"
+  my_array.unshift("Sandra")
 end
 
 def remove_element_from_end_of_array(array)
-  
+   my_array.pop()
 end
 
 def remove_element_from_start_of_array(array)
